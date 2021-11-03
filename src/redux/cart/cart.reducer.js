340ac1830b694b3efb27411757db67e7e7243663
TurkishCoffee <alexxis.noyola@gmail.com>
2021-11-02 23:28:35 -0500

    Stripe payment, refactoring code

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,13 +1,13 @@
 import { CartActionTypes } from "./cart.types";
-import { addItemToCart } from './car.utils';
+import { addItemToCart, removeItemFromCart } from './car.utils';
 
 const INITIAL_STATE = {
     hidden: true,
     cartItems: []
- }
- 
- const cartReducer = (state = INITIAL_STATE, action) => {
-    switch(action.type) {
+}
+
+const cartReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
@@ -16,11 +16,23 @@ const INITIAL_STATE = {
         case CartActionTypes.ADD_ITEM:
             return {
                 ...state,
-                cartItems: addItemToCart(state.cartItems, action.payload) 
+                cartItems: addItemToCart(state.cartItems, action.payload)
+            }
+        case CartActionTypes.REMOVE_ITEM:
+            return{
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            }
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    cartItem => cartItem.id !== action.payload.id
+                )
             }
         default:
             return state;
     }
- }
+}
 
- export default cartReducer;
+export default cartReducer;
